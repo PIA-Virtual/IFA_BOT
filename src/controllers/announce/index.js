cdcb2682da5db8555f98ccd96c842c3598d549ef
Umar Farooq <admin@DESKTOP-CUU7BND-6.local>
2021-03-13 13:28:37 +0000

    announce

--- src/controllers/announce/index.js
+++ src/controllers/announce/index.js
@@ -1,7 +1,9 @@
 'use strict';
 const announceController = message => {
-  const mess = message.content;
-  message.channel.send(mess);
+  const mess = message.content.substring(10);
+  message.client.channels.get('326859705714475009').send({
+    mess,
+})
 };
 
 module.exports = announceController;
